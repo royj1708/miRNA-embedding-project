9f9aa891b81d391f34d28870a55c06c42e93fe1d
Roy Judes <roy.judes@khealth.ai>
2021-01-23 12:10:20 +0200

    merged similar functions, added feature importances for random forest

--- Classification.py
+++ Classification.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 import pandas as pd
 import numpy as np
+import imblearn
 
 
 def oversampling(features):
@@ -20,25 +21,16 @@ def oversampling(features):
     features_normal_over = features_normal.sample(count_class_tumor, replace=True)
     return pd.concat([features_tumor, features_normal_over], axis=0)
 
-def get_random_forest_predictions(train_features, test_features, train_labels, n_estimators):
-    random_forest = RandomForestClassifier(n_estimators=n_estimators)
-    random_forest.fit(train_features, train_labels)
-    predictions = random_forest.predict(test_features)
+
+def get_classifier_predictions(classifier, train_features, test_features, train_labels):
+    classifier.fit(train_features, train_labels)
+    predictions = classifier.predict(test_features)
     return predictions
 
 
-def get_svc_predictions(train_features, test_features, train_labels):
-    svc = SVC(kernel='linear')
-    svc.fit(train_features, train_labels)
-    predictions = svc.predict(test_features)
-    return predictions
+def get_feature_importance_list(classifier):
+    return list(classifier.feature_importances_)
 
-
-def get_naive_bayes_predictions(train_features, test_features, train_labels):
-    naive_bayes = GaussianNB()
-    naive_bayes.fit(train_features, train_labels)
-    predictions = naive_bayes.predict(test_features)
-    return predictions
 
 def get_metrics_scores(test_labels, predictions):
     """
@@ -67,42 +59,64 @@ def get_metrics_scores(test_labels, predictions):
     results['auc'] = metrics.roc_auc_score(test_labels_auc, predictions_auc)
     return results
 
+
 def main(samples_csv_path):
     features = pd.read_csv(samples_csv_path)
-    #oversampling - optional
-    features = oversampling(features)
+    # oversampling - optional
+    # features = oversampling(features)
     # Labels are the values we want to predict
     labels = np.array(features['sample_type'])
     # Remove the labels from the features
     # axis 1 refers to the columns
-    features= features.drop('sample_type', axis=1)
+    features = features.drop('sample_type', axis=1)
     # Saving feature names for later use
     feature_list = list(features.columns)
     # Convert to numpy array
     features = np.array(features)
 
     # Split the data into training and testing sets
-    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.3,stratify=labels)
+    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.3,
+                                                                                stratify=labels)
 
     # print('Training Features Shape:', train_features.shape)
     # print('Training Labels Shape:', train_labels.shape)
     # print('Testing Features Shape:', test_features.shape)
     # print('Testing Labels Shape:', test_labels.shape)
-    results_random_forest = get_metrics_scores(test_labels, get_random_forest_predictions(train_features, test_features, train_labels, 100))
+
+    # -------------------------------------------- Random Forest --------------------------------------------
+    random_forest = RandomForestClassifier(n_estimators=100)
+    random_forest_predictions = get_classifier_predictions(random_forest, train_features, test_features, train_labels)
+    results_random_forest = get_metrics_scores(test_labels, random_forest_predictions)
+    random_forest_feature_importance = get_feature_importance_list(random_forest)
+    feature_importance_dict = {}
+    for index, feature in enumerate(feature_list):
+        feature_importance_dict[feature] = random_forest_feature_importance[index]
+
     print("--Random Forest model--")
     print(f"accuracy: {results_random_forest['accuracy']}")
     print(f"recall normal: {results_random_forest['recall_normal']}")
     print(f"recall tumor: {results_random_forest['recall_tumor']}")
     print(f"auc: {results_random_forest['auc']}", "\n")
+    print("Feature Importance:\n", random_forest_feature_importance, "\n")
+    for index, feature in enumerate(sorted(feature_importance_dict, key=feature_importance_dict.get, reverse=True)):
+        print(f"MIR: {feature}, Importance: {feature_importance_dict[feature]}")
+        if index == 10:
+            break
 
-    results_svc = get_metrics_scores(test_labels, get_svc_predictions(train_features, test_features, train_labels))
+    # -------------------------------------------- SVC --------------------------------------------
+    svc = SVC(kernel='linear')
+    svc_predictions = get_classifier_predictions(svc, train_features, test_features, train_labels)
+    results_svc = get_metrics_scores(test_labels, svc_predictions)
     print("--SVC model--")
     print(f"accuracy: {results_svc['accuracy']}")
     print(f"recall normal: {results_svc['recall_normal']}")
     print(f"recall tumor: {results_svc['recall_tumor']}")
     print(f"auc: {results_svc['auc']}", "\n")
 
-    results_naive_bayes = get_metrics_scores(test_labels, get_naive_bayes_predictions(train_features, test_features, train_labels))
+    # -------------------------------------------- Naive Bayes --------------------------------------------
+    naive_bayes = GaussianNB()
+    naive_bayes_predictions = get_classifier_predictions(naive_bayes, train_features, test_features, train_labels)
+    results_naive_bayes = get_metrics_scores(test_labels, naive_bayes_predictions)
     print("--Naive Bayes model--")
     print(f"accuracy: {results_naive_bayes['accuracy']}")
     print(f"recall normal: {results_naive_bayes['recall_normal']}")
@@ -110,11 +124,11 @@ def main(samples_csv_path):
     print(f"auc: {results_naive_bayes['auc']}")
 
 
-kidney_samples_csv_path = "C:\\Users\\Naor\\Google Drive\\שנה ד'\\פרויקט גמר\\profiles\\Bronchus_and_lung\\bronchus_and_lung_samples.csv"
-kidney_embedded_samples_csv_path = "C:\\Users\\Naor\\Google Drive\\שנה ד'\\פרויקט גמר\\profiles\Kidney\\embedded_profiles.csv"
+# kidney_samples_csv_path = "C:\\Users\\Naor\\Google Drive\\שנה ד'\\פרויקט גמר\\profiles\\Bronchus_and_lung\\bronchus_and_lung_samples.csv"
+# kidney_embedded_samples_csv_path = "C:\\Users\\Naor\\Google Drive\\שנה ד'\\פרויקט גמר\\profiles\Kidney\\embedded_profiles.csv"
 
-# kidney_embedded_samples_csv_path = 'embedded_profiles_0_300_5.csv'
-# kidney_samples_csv_path = '/Users/royjudes/Desktop/miRNA embedding project/kidney_samples.csv'
+kidney_embedded_samples_csv_path = 'embedded_profiles_0_300_5.csv'
+kidney_samples_csv_path = '/Users/royjudes/Desktop/miRNA embedding project/kidney_samples.csv'
 
 print("<<<<<<<<<<< RESULTS WITHOUT EMBEDDING: >>>>>>>>>>>")
 main(kidney_samples_csv_path)
